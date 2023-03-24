b7158ce312785b6fef83dcf773dbfe3cfea932b3
eduargenton <eduardov.argenton@outlook.com>
2023-03-24 15:33:32 -0300

    AllowedHost Server

--- lpn/settings.py
+++ lpn/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1!ocf63_ghs@fx^57meq^n%uo=ymz)6@ibbi494k9amzuty=@n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['206.189.207.71', 'localhost']
 
 
 # Application definition
